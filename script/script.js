27a7b28174515b75464595833104c97e12038552
Yulia Ovchinnikova <yulia.ovchinnikova.92@gmail.com>
2021-10-19 00:30:06 +0300

    Fix: improve js after review

--- script/script.js
+++ script/script.js
@@ -1,69 +1,89 @@
 // Profile elements
-let editButton = document.querySelector('.profile__edit-button');
-let userName = document.querySelector('.profile__name');
-let userDescription = document.querySelector('.profile__description');
-let addButton = document.querySelector('.profile__add-button');
+const editButton = document.querySelector('.profile__edit-button');
+const userName = document.querySelector('.profile__name');
+const userDescription = document.querySelector('.profile__description');
+const addButton = document.querySelector('.profile__add-button');
 
 // Popup to edit profile elements
-let popupEdit = document.querySelector('.popup_edit');
-let formEdit = popupEdit.querySelector('.popup__form');
-let nameInputValue = formEdit.querySelector('.popup__input_type_name');
-let descriptionInputValue = formEdit.querySelector(
+const popupEdit = document.querySelector('.popup_edit');
+const formEdit = popupEdit.querySelector('.popup__form');
+const nameInputValue = formEdit.querySelector('.popup__input_type_name');
+const descriptionInputValue = formEdit.querySelector(
   '.popup__input_type_description'
 );
-let closeButtons = document.querySelectorAll('.popup__close-button');
+const closeButtons = document.querySelectorAll('.popup__close-button');
+
+const popups = document.querySelectorAll('.popup');
 
 // Popup to add place elements
-let popupAddPlace = document.querySelector('.popup_adding');
-let formAddPlace = popupAddPlace.querySelector('.popup__form');
-let placeNameInputValue = popupAddPlace.querySelector(
+const popupAddPlace = document.querySelector('.popup_adding');
+const formAddPlace = popupAddPlace.querySelector('.popup__form');
+const placeNameInputValue = popupAddPlace.querySelector(
   '.popup__input_type_city'
 );
-let placeLinkInputValue = popupAddPlace.querySelector(
+const placeLinkInputValue = popupAddPlace.querySelector(
   '.popup__input_type_link'
 );
 
 // Popup to open photo
-let popupOpenPhoto = document.querySelector('.popup_photo');
-let openedPhotoName = popupOpenPhoto.querySelector('.popup__figcaption');
-let openedPhoto = popupOpenPhoto.querySelector('.popup__opened-image');
+const popupOpenPhoto = document.querySelector('.popup_photo');
+const openedPhotoName = popupOpenPhoto.querySelector('.popup__figcaption');
+const openedPhoto = popupOpenPhoto.querySelector('.popup__opened-image');
 
 // Places elements
 const placeTemplate = document.querySelector('#place').content;
-let placeContainer = document.querySelector('.places');
+const placeContainer = document.querySelector('.places');
 
 const initialCards = [
   {
     name: 'Красноярск',
     link: './images/krasnoyarsk.jpeg',
+    alt: 'Склон, на котором растет хвойный хеленый лес в Красноярске',
   },
   {
     name: 'Берген',
     link: './images/bergen.jpeg',
+    alt: 'Храм в Бергене на переднем плане, позади холмы и туман',
   },
   {
     name: 'Стокгольм',
     link: './images/stockholm.jpeg',
+    alt: 'Станция метро в Стокгольме в скале',
   },
   {
     name: 'Амстердам',
     link: './images/amsterdam.jpeg',
+    alt: 'Лодка на канале в Амстердаме',
   },
   {
     name: 'Хельсинки',
     link: './images/helsinki.jpeg',
+    alt: 'Памятник Сибелиусу в Хельсинки',
   },
   {
     name: 'Цюрих',
     link: './images/zurich.jpeg',
+    alt: 'Вид со смотровой площадки на центр города Цюрих',
   },
 ];
 
-function creacteCard(name, link) {
+function creacteCard(name, link, alt) {
   const placeElement = placeTemplate.querySelector('.place').cloneNode(true);
-  placeElement.querySelector('.place__image').src = link;
-  placeElement.querySelector('.place__image').alt = name;
+  const placeImage = placeElement.querySelector('.place__image');
+
+  placeImage.src = link;
+  if (alt) {
+    placeImage.alt = alt;
+  } else {
+    placeImage.alt = name;
+  }
   placeElement.querySelector('.place__name').textContent = name;
+
+  placeImage.addEventListener('click', (event) => {
+    openedPhotoName.textContent = event.target.alt;
+    openedPhoto.src = event.target.src;
+    openPopup(popupOpenPhoto);
+  });
 
   placeElement
     .querySelector('.place__like')
@@ -77,29 +97,24 @@ function creacteCard(name, link) {
       event.target.closest('.place').remove();
     });
 
-  placeElement
-    .querySelector('.place__image')
-    .addEventListener('click', (event) => {
-      openedPhotoName.textContent = event.target.alt;
-      openedPhoto.src = event.target.src;
-      openPopup(popupOpenPhoto);
-    });
-
   return placeElement;
 }
 
 initialCards.forEach(function (place) {
-  placeContainer.append(creacteCard(place.name, place.link));
+  placeContainer.append(creacteCard(place.name, place.link, place.alt));
 });
 
 function addCard(event) {
   event.preventDefault();
 
   placeContainer.prepend(
-    creacteCard(placeNameInputValue.value, placeLinkInputValue.value)
+    creacteCard(placeNameInputValue.value, placeLinkInputValue.value, undefined)
   );
 
   closePopup(popupAddPlace);
+
+  placeNameInputValue.value = '';
+  placeLinkInputValue.value = '';
 }
 
 function openPopup(popup) {
@@ -132,20 +147,14 @@ formEdit.addEventListener('submit', saveData);
 
 formAddPlace.addEventListener('submit', addCard);
 
-Array.from(closeButtons).forEach(function (closeButton) {
-  closeButton.addEventListener('click', (evt) => {
-    if (evt.target.closest('.popup_edit')) {
-      closePopup(popupEdit);
-    } else if (evt.target.closest('.popup_photo')) {
-      closePopup(popupOpenPhoto);
-    } else {
-      closePopup(popupAddPlace);
+popups.forEach((popup) => {
+  popup.addEventListener('click', (evt) => {
+    if (evt.target.classList.contains('popup__close-button')) {
+      closePopup(popup);
     }
   });
 });
 
 window.addEventListener('load', () => {
-  document
-    .querySelectorAll('.popup')
-    .forEach((popup) => popup.classList.add('popup_transition'));
+  popups.forEach((popup) => popup.classList.add('popup_transition'));
 });
