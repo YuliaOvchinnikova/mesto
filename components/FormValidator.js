6ec6bf7ab8beffb72dc03b0e89cfb4d646a868c3
Yulia Ovchinnikova <yulia.ovchinnikova.92@gmail.com>
2021-11-24 01:18:27 +0200

    Fix: delete console.log

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -71,7 +71,6 @@ export default class FormValidator {
 
     this._inputList.forEach((inputElement) => {
       inputElement.addEventListener('input', () => {
-        console.log(inputElement);
         this._checkInputValidity(inputElement);
         this._toggleButtonState();
       });
